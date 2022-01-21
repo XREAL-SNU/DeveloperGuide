85f12de64e5e133895265db0dc8109d1bcc36e32
Adam <cmlee3211@gmail.com>
2022-01-22 07:03:22 +0900

    Finish CurrentRoom

---
+++ 네트워크/Photon/Photon Networking 2/4.CurrentRoom/CreateRoomMenu.cs
@@ -0,0 +1,36 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Realtime;
+
+public class CreateRoomMenu : MonoBehaviourPunCallBacks {
+    [SerializeField]
+    private Text  _roomName;
+
+    private RoomsCanvases _roomsCanvases;
+
+    public void FirstInitialize(RoomsCanvases canvases) {
+        _roomsCanvases = canvases;
+    }
+
+    public void OnClick_CreateRoom() {
+        if(!PhotonNetwork.IsConnected()) {
+            return;
+        }
+
+        RoomOptions options = new RoomOptions();
+        options.maxPlayers = 4;
+        PhotonNetwork.JoinOrCreateRoom(_roomName.text, options, TypedLobby.Default);
+    }
+
+    public override void OnCreatedRoom() {
+		MasterManager.DebugConsole.AddText("Created Room Successfully", this);
+        _roomsCanvases.CurrentRoomCanvas.Show();
+    }
+
+	public override void OnCreatedRoomFailed(short returnCode, string message) {
+		MasterManager.DebugConsole.AddText("Room Creation Failed: " + message, this);	
+	}
+}
